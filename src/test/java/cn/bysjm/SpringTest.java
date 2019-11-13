6a0ef038a9a7875aabf091f7c246cc1906a0a6d2
sanjiaomaojl <1026845550@qq.com>
2019-11-13 16:25:42 +0800

    SSM

---
+++ src/test/java/cn/bysjm/SpringTest.java
@@ -0,0 +1,17 @@
+package cn.bysjm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class SpringTest {
+
+    @Test
+    public void test01() throws Exception {
+
+    }
+
+}
